0350fd7f3f7b0ba3e00024bc3d6191d28aff680f
Luc-Ay <ayomideayomipo147@gmail.com>
2025-06-05 13:03:06 +0200

    updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,40 @@
 import User from './../models/User.js';
-import bcrypt from 'bcryptjs';
-import { generateToken } from './../lib/utils.js';
 import cloudinary from './../lib/cloudinary.js';
 
 
-export const userProfile = async (params) => {}
+export const userProfile = async (req,res) => {
+    try {
+        const userId = req.params.id;
+
+        if(!userId){
+            return res.status(400).json({ 
+                Message: 'User ID is required.', 
+                Success: false, 
+            });
+        }
+
+         const user = await User.findById(userId).select('-password');
+
+        if (!user) {
+            return res.status(404).json({ 
+                Message: 'User not found.', 
+                Success: false, 
+            });
+    }   
+
+    return res.status(200).json({
+        Success: true, 
+        data: user,
+    })
+    } catch (error) {
+        console.error('Error fetching user profile:', error);
+        return res.status(500).json({ 
+            success: false, 
+            message: 'Server error.',
+
+         });
+    }
+}
 export const updateProfile = async (req,res) => {
     try {
         const {profilePic} = req.body;
