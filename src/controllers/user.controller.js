4df38c3c0971f5193bd0e36d3e5749728f810a56
Luc-Ay <ayomideayomipo147@gmail.com>
2025-06-05 12:43:29 +0200

    updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,38 @@
 import User from './../models/User.js';
 import bcrypt from 'bcryptjs';
 import { generateToken } from './../lib/utils.js';
+import cloudinary from './../lib/cloudinary.js';
 
 
 export const userProfile = async (params) => {}
-export const updateProfile = async (params) => {}
+export const updateProfile = async (req,res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({
+                Message: "Profile pic is required",
+                success: false,
+            })
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updateUser = await User.findByIdandUpdate(
+            userId,
+            {profilePic: uploadResponse.secure_url},
+            {new: true}
+        );
+
+        res.status(200).json(updateUser)
+    } catch (error) {
+        console.log("error in update profile", error)
+        res.status(500).json({
+            message: "Internal server error",
+            success: true,
+        })
+    }
+}
 
 
 export const logoutUser = async (req,res) => {
