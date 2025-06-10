5310341c63839465693bce06390645022d901578
Luc-Ay <ayomideayomipo147@gmail.com>
2025-06-10 17:56:51 +0200

    update

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/users', protectRoute, getUserForSideBar)
 router.get('/:id', protectRoute, getMessages)
 router.post('/send.:id', protectRoute, sendMessage);
+router.put('/', (req,res))
 
 
 export default router;
