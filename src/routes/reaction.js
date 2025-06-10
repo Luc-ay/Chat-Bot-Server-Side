6231b067aff3f5063a8dfb22323f373aedd2d238
Luc-Ay <ayomideayomipo147@gmail.com>
2025-06-10 18:01:54 +0200

    updated

--- src/routes/reaction.js
+++ src/routes/reaction.js
@@ -1,1 +1,15 @@
 // This is for reaction routes
+
+import express from 'express'
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { getMessages, getUserForSideBar } from '../controllers/message.controller.js';
+
+const router = express.Router();
+
+router.get('/users', protectRoute, getUserForSideBar)
+router.get('/:id', protectRoute, getMessages)
+router.post('/send.:id', protectRoute, sendMessage);
+router.put('/', (req,res))
+
+
+export default router;
